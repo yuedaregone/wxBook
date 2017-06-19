af9cffac7994071e391aff0c760d7a66fccd34bb
yuedaregone <810566249@qq.com>
2017-06-19 19:34:20 +0800

    add cmakelist file

--- wxBook/src/Utility.cpp
+++ wxBook/src/Utility.cpp
@@ -10,7 +10,7 @@ unsigned long Utility::str2hex(const char * str)
 	unsigned long ret = 0;
 
 	for (int i = strlen(str) - 1; i >= 0; --i)
-	{	
+	{
 		char ch = str[i];
 		if (ch >= '0' && ch <= '9')
 		{
@@ -42,4 +42,61 @@ std::string Utility::GetCurPath()
 	size_t last = str.find_last_of('\\');
 
 	return str.substr(0, last);
+}
+
+std::string Utility::GetExtenName(std::string p)
+{
+	return p.substr(p.find_last_of("."), p.length());
+}
+
+void Utility::GetFiles(std::vector<std::string> &files, std::string &dir, const char* suffix)
+{
+    WIN32_FIND_DATA file;
+    HANDLE lf;
+    if ((lf = ::FindFirstFile(dir.c_str(), &file)) == INVALID_HANDLE_VALUE) {
+        std::cout << dir << " not found!!!" << std::endl;
+    } else {
+        while(::FindNextFile(lf, &file) == 0) {
+            if (strcmp((char*)file.cFileName, ".") == 0 || strcmp((char*)file.cFileName, "..") == 0)
+                continue;
+			std::string exten = Utility::GetExtenName((char*)file.cFileName);
+			if (suffix != NULL && strcmp(exten.c_str(), suffix) != 0)
+				continue;
+
+            files.push_back((char*)file.cFileName);
+        }
+    }
+    FindClose(lf);
+}
+
+void Utility::FindAllFile(std::vector<std::string>& strFiles, std::string lpPath, const char* suffix)
+{
+	WIN32_FIND_DATA FindFileData;
+	std::string szFind = lpPath + "\\*.*";
+
+	HANDLE hFind = ::FindFirstFile(szFind.c_str(), &FindFileData);
+	if (INVALID_HANDLE_VALUE == hFind) return;
+	while (::FindNextFile(hFind, &FindFileData))
+	{
+		if (strcmp((char*)FindFileData.cFileName, ".") == 0 || strcmp((char*)FindFileData.cFileName, "..") == 0)
+		{
+			continue;
+		}
+		std::string szFile = lpPath + "\\";
+		szFile = szFile + FindFileData.cFileName;
+
+		if (FindFileData.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
+		{
+			FindAllFile(strFiles, szFile);
+		}
+		else
+		{
+			std::string exten = Utility::GetExtenName(FindFileData.cFileName);
+			if (strcmp(exten.c_str(), suffix) == 0)
+			{
+				strFiles.push_back(szFile);
+			}
+		}
+	}
+	FindClose(hFind);
 }
--- wxBook/src/Utility.h
+++ wxBook/src/Utility.h
@@ -1,9 +1,17 @@
 #pragma once
 #include <iostream>
-
+#include <vector>
+#include <string>
 class Utility
 {
 public:
 	static unsigned long str2hex(const char* str);
+
 	static std::string GetCurPath();
+
+	static void GetFiles(std::vector<std::string> &files, std::string &dir, const char* suffix = NULL);
+
+	static std::string GetExtenName(std::string p);
+
+	static void FindAllFile(std::vector<std::string>& strFiles, std::string lpPath, const char* suffix = NULL);
 };
